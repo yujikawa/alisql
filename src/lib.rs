ae1711048ce727babb8d256bd6d12a02b35af05d
yujikawa <ykyujikawa@gmail.com>
2022-10-30 08:04:02 +0900

    Add orientation to mermaid graph

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,65 @@ use walkdir::WalkDir;
 
 /// Get dependencies from SQL written ref macro with Jinja.
 /// # Example
+/// ```rust
 /// let d: Vec<Table> = get_dependencies("sqls");
+/// ```
+/// First, create sql like Jinja.
+/// ```sql
+/// -- src/sample_sqls/sample.sql
+/// select 
+/// u.*
+/// , r.* 
+/// from {{ ref("db", "users") }} as u
+/// left join {{ ref("role") }} as r on
+/// u.id = r.user_id
+/// ```
+///
+/// ```sql
+/// -- src/sample_sqls/sample2.sql
+/// select 
+/// u.*
+/// , r.* 
+/// from {{ ref("db", "sales") }} as u
+/// left join {{ ref("db", "sale_detail") }} as r on
+/// u.id = r.sale_id
+/// ```
+///
+/// Use alias lib from main function. 
+/// ```rust
+/// use alias;
+/// fn main() {
+///     let d = alias::get_dependencies("src/sample_sqls") 
+/// }
+/// ```
+/// ## Result
+///
+/// ```rust
+/// d = [
+///     Table {
+///         table: "sample",
+///         sql: SQL {
+///             path: "src/sample_sqls/sample.sql",
+///             query: "select \nu.*\n, r.* \nfrom {{ ref(\"db\", \"users\") }} as u\nleft join {{ ref(\"role\") }} as r on\nu.id = r.user_id",
+///         },
+///         depends_on: [
+///             "db.users",
+///             "role",
+///         ],
+///     },
+///     Table {
+///         table: "sample2",
+///         sql: SQL {
+///             path: "src/sample_sqls/sample2.sql",
+///             query: "select \nu.*\n, r.* \nfrom {{ ref(\"db\", \"sales\") }} as u\nleft join {{ ref(\"db\", \"sale_detail\") }} as r on\nu.id = r.sale_id",
+///         },
+///         depends_on: [
+///             "db.sales",
+///             "db.sale_detail",
+///         ],
+///     },
+/// ]
+/// ```
 pub fn get_dependencies(root_dir: &str) -> Vec<Table> {
     let mut v: Vec<Table> = Vec::new();
     for entry in WalkDir::new(root_dir) {
@@ -33,9 +91,28 @@ pub fn get_dependencies(root_dir: &str) -> Vec<Table> {
 
 /// Get dependencies graph from SQL with Mermaid.
 /// # Example
-/// let mermaid = get_mermaid("sqls");
-pub fn get_mermaid(root_dir: &str) -> String {
+/// ```rust
+/// let mermaid = get_mermaid("sqls", "TD");
+/// ```
+/// orientation are
+/// - TB : top to bottom
+/// - TD : top-down/ same as top to bottom
+/// - BT : bottom to top
+/// - RL : right to left
+/// - LR : left to right
+/// 
+/// Reference: [Mermaid](https://mermaid-js.github.io/mermaid/#/./flowchart?id=flowchart-orientation)
+/// 
+/// ## Result
+/// ```mermaid
+///graph TD;
+///db.users --> sample;
+///role --> sample;
+///db.sales --> sample2;
+///db.sale_detail --> sample2;
+///```
+pub fn get_mermaid(root_dir: &str, orientation: &str) -> String {
     let tables = get_dependencies(root_dir);
     let m = Mermaid::new(tables);
-    m.get_graph()
+    m.get_graph(orientation)
 }
--- src/sql_analyzer/graph.rs
+++ src/sql_analyzer/graph.rs
@@ -9,9 +9,9 @@ impl Mermaid {
         Mermaid { tables: tables }
     }
 
-    pub fn get_graph(&self) -> String {
+    pub fn get_graph(&self, orientation: &str) -> String {
         let mut temp: Vec<String> = Vec::new();
-        temp.push("graph TD".to_string());
+        temp.push(format!("graph {}", orientation).to_string());
         for table in &self.tables {
             for depends_table in &table.depends_on {
                 temp.push(format!("{} --> {}", depends_table, table.table));
@@ -33,7 +33,7 @@ mod tests {
         let s = RegexSQLAnalyser::new("src/sample_sqls/sample.sql".to_string());
         let tables = s.get_analized_table();
         let m = Mermaid::new(vec![tables]);
-        let mermaid = m.get_graph();
+        let mermaid = m.get_graph("TD");
         assert!(mermaid == "graph TD;\ndb.users --> sample;\nrole --> sample;\n".to_string());
     }
 }
